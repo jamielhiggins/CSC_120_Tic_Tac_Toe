c43874d38834bcdcb293840ce147d9b64e3f64f1
jamielhiggins <jlhiggins3@my.waketech.edu>
2022-04-16 21:40:42 -0400

    Board Print Function added

---
+++ board.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    board1 = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]]
+    print_board(board1)
+
+def print_board(board):
+    for row in board:
+        print(row)
+
+#validating function that checks if the mark can be placed
+#def check_mark():
+
+#places a mark on the board
+#def place_mark():
+
+#def check_win():
+
+main()
